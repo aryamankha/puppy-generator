dde740b6148b31ac4d83c89b46c84e649b8c43c0
Aryaman Khandelwal <aryamankhandelwal@Aryamans-MacBook-Pro.local>
2023-11-10 15:34:21 -0500

    increase navbar height

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -26,19 +26,24 @@ export const NavBar = () => {
   
     return (
         <div
-          className={`flex flex-row w-full h-fit ${
+          className={` ${
             gameState ? "justify-between" : "justify-end"
-          } px-10 pt-5 pb-1 overflow-hidden`}
+          } flex flex-row w-full h-fit px-10 pt-5 overflow-hidden pb-3`}
         >
-          {gameState && <div className = "flex flex-col"><p>Streak: {streak}</p><p>High Score: {maxStreak}</p></div>}
-          <Switch.Root
-            className="w-[42px] h-[25px] bg-blackA6 rounded-full relative shadow-[0_2px_10px] shadow-blackA4 focus:shadow-[0_0_0_2px] focus:shadow-black data-[state=checked]:bg-black outline-none cursor-default"
-            id="airplane-mode"
-            style={{ "-webkit-tap-highlight-color": "rgba(0, 0, 0, 0)" }}
-            onCheckedChange={switchHandler}
-          >
-            <Switch.Thumb className="block w-[21px] h-[21px] bg-white rounded-full shadow-[0_2px_2px] shadow-blackA4 transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]" />
-          </Switch.Root>
+         {gameState && (
+           <div className="flex flex-col">
+             <p>Streak: {streak}</p>
+             <p>High Score: {maxStreak}</p>
+           </div>
+         )}
+         <Switch.Root
+           className="w-[42px] h-[25px] bg-blackA6 rounded-full relative shadow-[0_2px_10px] shadow-blackA4 focus:shadow-[0_0_0_2px] focus:shadow-black data-[state=checked]:bg-black outline-none cursor-default"
+           id="airplane-mode"
+           style={{ "-webkit-tap-highlight-color": "rgba(0, 0, 0, 0)" }}
+           onCheckedChange={switchHandler}
+         >
+           <Switch.Thumb className="block w-[21px] h-[21px] bg-white rounded-full shadow-[0_2px_2px] shadow-blackA4 transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]" />
+         </Switch.Root>
         </div>
     )
 }
